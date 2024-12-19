330c21a055c687d62194c7d86d3455fec66a2fd2
Sourav Kalal <kalalsourav18@gmail.com>
2024-12-19 21:28:21 +0530

    updated RSA enc script for new version

--- RSA/rsa-enc.js
+++ RSA/rsa-enc.js
@@ -12,13 +12,39 @@ const options = program.opts();
  
 const filePath = options.data;
 const absoluteFilePath = path.resolve(filePath);
-var data = fs.readFileSync(absoluteFilePath).toString();
-const jsonData = JSON.parse(data);
-const base64Data = jsonData.data; 
-const requestbody = Buffer.from(base64Data, 'base64').toString('utf8');
+var data = fs.readFileSync(absoluteFilePath, 'utf8')
+const bodyEndMarker = '\n--BODY_END--\n';
+const [byteArrayStr, headersRaw] = data.split(bodyEndMarker);
 
-let crypt = new JSEncrypt();
-let PASS_PUB_KEY = '<<<public key>>'
-crypt.setPublicKey(PASS_PUB_KEY);
-let encrypted_data = crypt.encrypt(requestbody);
-console.log(encodeURIComponent(encrypted_data))
+const byteArray = JSON.parse(byteArrayStr.trim())
+const buffer = Buffer.from(byteArray); // Convert byte array to Buffer
+const plaintext = buffer.toString('utf8') // convert it string 
+
+
+const publicKey  = "-----BEGIN PUBLIC KEY-----\n<>-----END PUBLIC-----\n"; 
+// call the functions to handle decrpytion, headers 
+const originalText = encryptData(plaintext,publicKey);
+
+const updatedHeader = Read_parse_Header(headersRaw);
+
+// convert the updated string to byte array again
+const updated_output_byte = Array.from(originalText).map(char => char.charCodeAt(0));
+var output = updated_output_byte +"\n--BODY_END--\n"+updatedHeader
+// write to same temp file in same formamt body\n--BODY_END--\nheader
+fs.writeFileSync(absoluteFilePath,output)
+
+
+
+function encryptData(data, publicKey) {
+  let crypt = new JSEncrypt();
+  crypt.setPublicKey(publicKey);
+  return crypt.encrypt(data);
+}
+
+function Read_parse_Header(headersRaw) {
+
+  // logic to read/edit header
+  
+  return headersRaw;
+  
+  }
